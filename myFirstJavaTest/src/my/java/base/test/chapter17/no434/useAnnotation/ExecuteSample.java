d63c1f0405600bedf7bc7896a69a444073afd8e1
inokou <inokou@10.229.72.19>
2017-09-01 14:34:06 +0900

    初回作成

---
+++ myFirstJavaTest/src/my/java/base/test/chapter17/no434/useAnnotation/ExecuteSample.java
@@ -0,0 +1,19 @@
+package my.java.base.test.chapter17.no434.useAnnotation;
+public class ExecuteSample {
+  public static void main(String[] args) {
+    /* ExecuteLevelアノテーションを指定したメソッドを呼び出す */
+    ExecuteSample logSample = new ExecuteSample();
+    logSample.method01();
+    logSample.method02();
+  }
+
+  @ExecuteLevel(level=1)
+  public void method01() {
+    System.out.println("method01を実行");
+  }
+
+  @ExecuteLevel(level=2)
+    public void method02() {
+    System.out.println("method02を実行");
+  }
+}
